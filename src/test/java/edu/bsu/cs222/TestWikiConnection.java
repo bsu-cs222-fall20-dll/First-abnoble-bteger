eb82f67c7332e2f841febc80af468550e4ad1c5b
Aric Noble <abnoble@bsu.edu>
2020-10-05 18:16:14 -0400

    Refactor TestWikiConnection to remove production code from tests

--- src/test/java/edu/bsu/cs222/TestWikiConnection.java
+++ src/test/java/edu/bsu/cs222/TestWikiConnection.java
@@ -9,26 +9,18 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestWikiConnection {
-
+    final private WikiConnection CONNECTION = new WikiConnection();
+    final private String SAMPLE_URL_STRING = "https://en.wikipedia.org/w/api.php?" +
+            "action=query&format=json&" +
+            "prop=revisions&titles=Zappa&" +
+            "rvprop=timestamp|user&rvlimit=20&redirects";
     @Test
-    public void sendGetRequestTest() throws IOException {
-        WikiConnection connection = new WikiConnection();
-        URL testUrl = new URL("https://en.wikipedia.org/w/api.php?" +
-                "action=query&format=json&" +
-                "prop=revisions&titles=Zappa&" +
-                "rvprop=timestamp|user&rvlimit=4&redirects");
-        InputStream inputStream = connection.pullRevisionData(testUrl);
-        Assertions.assertNotNull(inputStream);
+    public void pullRevisionDataTest() throws IOException {
+        Assertions.assertNotNull(CONNECTION.pullRevisionData(new URL(SAMPLE_URL_STRING)));
     }
 
     @Test
     public void generateHTTPRequestTest() throws MalformedURLException {
-        WikiConnection wikiConnection = new WikiConnection();
-        URL getRequest = wikiConnection.generateHTTPRequest("Zappa");
-        String SAMPLE_GET_REQUEST = "https://en.wikipedia.org/w/api.php?" +
-                "action=query&format=json&" +
-                "prop=revisions&titles=Zappa&" +
-                "rvprop=timestamp|user&rvlimit=20&redirects";
-        Assertions.assertEquals(SAMPLE_GET_REQUEST, getRequest.toString());
+        Assertions.assertEquals(SAMPLE_URL_STRING, CONNECTION.generateHTTPRequest("Zappa").toString());
     }
 }
