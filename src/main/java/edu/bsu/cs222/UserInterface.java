166cad48d0ba523a5ec8c4d3e614ca8c693f4add
Aric Noble <abnoble@bsu.edu>
2020-10-05 20:17:34 -0400

    Redesign GUI to output wiki revision results
    This commit refactored the GUI code to be cleaner and added functionality to the
    search button. Now our project outputs the latest revisions of a specified Wiki
    page in reverse chronological order.

--- src/main/java/edu/bsu/cs222/UserInterface.java
+++ src/main/java/edu/bsu/cs222/UserInterface.java
@@ -1,21 +1,6 @@
 package edu.bsu.cs222;
 
-import java.util.ArrayList;
-
 public class UserInterface {
-
-
-   public String requestWikiSearch() {
-       return null;
-   }
-
-    public void showMostRecentRevisions(ArrayList<Revision> revisions) {
-        System.out.println("Here are the most recent changes to the page:");
-        for (int i = 0; i < revisions.size(); i++) {
-            System.out.printf("\t%d.)\t%s\n", i+1, revisions.get(i).getUser());
-            System.out.printf("\t    \t%s\n\n", revisions.get(i).getTimeStamp());
-        }
-    }
 
     public void showRedirection(String original, String redirected) {
         System.out.printf("Search was redirected from %s to %s\n", original, redirected);
